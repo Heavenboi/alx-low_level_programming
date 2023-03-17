dc9afd3e19d43a4be8854a588f156aeb29666858
Heavenboi <nigelzenda98@gmail.com>
2023-03-17 02:11:14 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put fuction 
+ * this program returns zero and print "programing is like building a muiltilingual puzzle"
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
+
