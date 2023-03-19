3bc85a8bb583d5bb2a03dbd03a9c1124f7fdc4ac
Heavenboi <nigelzenda98@gmail.com>
2023-03-19 11:07:58 -0700

    correcting some erros avoind the betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print thr  string in the put function 
+ * main - print the put function
  *
- * using the main function 
- * this program prints "with proper grammar, but the outcome is a piece of srt"
+ * Description: use the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
