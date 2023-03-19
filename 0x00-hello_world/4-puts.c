ea7a84a46f58c6d7269b062c8d1ea09dc74f0349
Heavenboi <nigelzenda98@gmail.com>
2023-03-19 02:00:36 -0700

    corrects

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print the string in the put fuction 
- * this program returns zero and print "programing is like building a muiltilingual puzzle"
+ *Rerturn: this program returns zero and print 'programing is like building a muiltilingual puzzle'
  */
 
 int main(void)
