3b1a318862a655f8d2e9e63ded6a2dba92ff9d7e
Heavenboi <nigelzenda98@gmail.com>
2023-03-23 14:05:00 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - check if numbers are 0 - 9
+0-main.c
+1-isdigit.c
+1-main.c
+}
+* * * */
+@c: char to be checked Return: 0 or 1
+int _isdigit(int c) {
+if (c >= '0' && c <= '9') return (1);
+else
+return (0);
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_numbers - print 0-9 * return: void
+2-mul.c
+2-main.c
+3-print_numbers.c
+* return: void */
+void print_numbers(void) {
+char c;
+for (c = '0'; c <= '9'; c++) {
+_putchar(c); }
+_putchar('\n'); }
