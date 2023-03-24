936754dd984ff6c104c98ad943a105959a02b1c9
Heavenboi <nigelzenda98@gmail.com>
2023-03-24 10:50:49 -0700

    All bugs fixed

--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -1,8 +1,14 @@
 #include "main.h"
+
 /**
-* main - check the code *
-* Return: Always 0. */
-int main(void) {
-more_numbers();
-return (0); }
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    more_numbers();
+    return (0);
 }
+
+
