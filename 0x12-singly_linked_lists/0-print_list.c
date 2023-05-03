e6d4395569f4e0833118ef9a6c97cc390607bf1d
Heavenboi <nigelzenda98@gmail.com>
2023-05-03 07:44:02 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
@@ -20,7 +19,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
-
 	return (s);
 }
 
