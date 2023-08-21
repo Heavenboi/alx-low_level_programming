61d86c323036c87a6936c71a705464f537303abf
Heavenboi <nigelzenda98@gmail.com>
2023-08-21 07:35:55 -0700

    dynamic library libdynamic.so containing all the functions listed below:

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: the initial segment of
+ *
+ * @accept: which consist only of bytes from
+ *
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] != 32)
+		{
+			for (b = 0; accept[b] != '\0'; b++)
+			{
+				if (s[a] == accept[b])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+		return (z);
+
+}
