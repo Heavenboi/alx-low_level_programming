85ff7e0a142048021fc2b9f3852512aace088de9
Heavenboi <nigelzenda98@gmail.com>
2023-09-10 12:28:21 -0700

    removed white space

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_create - Creates a hash table.
- * @size: The size of the hash table.
+ * @size: The size of the hash table
  *
  * Return: A pointer to the newly created hash table, or NULL if it fails.
  */
