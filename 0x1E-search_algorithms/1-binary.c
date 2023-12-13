9310f319aeb60fb6ef3f22d826b8b486dbe43fed
Heavenboi <nigelzenda98@gmail.com>
2023-12-13 06:47:10 +0200

    0-linear and fixed bugs

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,27 +1,39 @@
 #include "search_algos.h"
 
 /**
-* linear_search - searches for a value in an array of integers using the
-* Linear search algorithm
+* binary_search - searches for a value in a sorted array of integers using the
+* Binary search algorithm
 * @array: pointer to the first element of the array to search in
 * @size: number of elements in array
-* @value: value to search for
-* Return: if value not present in array return -1, else return first index
+* @value: the value to search for
+* Return: if value not present in array or if array is NULL -1, else
+* return first index where value is located
 */
-
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	size_t h = 0;
+	size_t h, r, l;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (h < size)
+	for (l = 0, h = size - 1; h >= l;)
 	{
-		printf("Value checked array[%lu] = [%d]\n", h, array[h]);
-		if (array[h] == value)
-			return (h);
-		h++;
+		printf("Searching in array: ");
+		for (r = l; r <= h; r++)
+		{
+			printf("%d", array[r]);
+			if (r < h)
+				printf(", ");
+		}
+		printf("\n");
+
+		r = l + (h - l) / 2;
+		if (array[r] == value)
+			return (r);
+		else if (array[r] > value)
+			h = r - 1;
+		else
+			l = r + 1;
 	}
 
 	return (-1);
